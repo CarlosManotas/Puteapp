15a4b32e56eb763db3b07b4d2775d2d731a79aa4
Carlos <Carlos>
2015-11-09 17:55:37 -0300

    agregando pagina 1

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ $(function(){
 	var $logo = $('.loader figure');
 	var $btn = $('#btn');
 	var $menu = $('[data="nav"]');
+	var $micro = $('[alt="micro"]');
 	$logo.animate({
 		'margin-top':'30em',
 		'opacity':'0'},2000,function(){
@@ -9,5 +10,10 @@ $(function(){
 		});
 	$btn.on('click',function(){
 		$menu.slideToggle();
+	})
+	$micro.on('click',function(){
+		$(this).css({
+			'opacity':'0'
+		});
 	})
 });
